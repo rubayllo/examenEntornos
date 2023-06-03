04f231fdd8da01e8baa05c56adda9dc33a3da5c4
Ruben Ayllon Martinez <ruben.ayllon@live.u-tad.com>
2023-06-03 11:00:47 +0200

    Tercer commit

--- ExamenMVC/src/main/java/vista/VistaSeleccionados.java
+++ ExamenMVC/src/main/java/vista/VistaSeleccionados.java
@@ -3,8 +3,6 @@ package vista;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,7 +11,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
@@ -21,7 +18,7 @@ import controlador.Controlador;
 import interfaceVistas.Vistas;
 import modelo.Modelo;
 
-public class VistaTablaDB extends JFrame implements Vistas{
+public class VistaSeleccionados extends JFrame implements Vistas{
 
 	private JPanel contentPane;
 	private Controlador controlador;
@@ -33,9 +30,7 @@ public class VistaTablaDB extends JFrame implements Vistas{
 	private JButton btnTb4;
 	private DefaultTableModel modeloTabla;
 	private JButton btnAad;
-	private JButton btnVerSel;
-	private JTextPane txtSelec;
-	private JScrollPane scrollPane_1;
+	private JButton btnVerTablas;
 
 	/**
 	 * Launch the application.
@@ -56,7 +51,7 @@ public class VistaTablaDB extends JFrame implements Vistas{
 	/**
 	 * Create the frame.
 	 */
-	public VistaTablaDB() {
+	public VistaSeleccionados() {
 		addWindowListener(new WindowAdapter() {
 			public void windowOpened(WindowEvent e) {
 				controlador.consultarQuery("SELECT * FROM world.city");
@@ -77,17 +72,6 @@ public class VistaTablaDB extends JFrame implements Vistas{
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
-		table.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				int fila = table.getSelectedRow();
-				int columnas = table.getColumnCount();
-				String seleccion = "";
-				for (int i = 1; i < columnas; i++) {
-					seleccion += table.getValueAt(fila, i-1) + " ";
-				}
-				setTxtSelec(seleccion);
-			}
-		});
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		scrollPane.setViewportView(table);
 		
@@ -97,7 +81,7 @@ public class VistaTablaDB extends JFrame implements Vistas{
 				controlador.consultarQuery("SELECT * FROM world.city");
 			}
 		});
-		btnTb1.setBounds(22, 194, 68, 29);
+		btnTb1.setBounds(22, 207, 68, 29);
 		contentPane.add(btnTb1);
 		
 		btnTb = new JButton("Tb2");
@@ -106,7 +90,7 @@ public class VistaTablaDB extends JFrame implements Vistas{
 				controlador.consultarQuery("SELECT * FROM world.country");
 			}
 		});
-		btnTb.setBounds(90, 194, 68, 29);
+		btnTb.setBounds(90, 207, 68, 29);
 		contentPane.add(btnTb);
 		
 		btnTb3 = new JButton("Tb3");
@@ -115,7 +99,7 @@ public class VistaTablaDB extends JFrame implements Vistas{
 				controlador.consultarQuery("SELECT * FROM world.countrylanguage");
 			}
 		});
-		btnTb3.setBounds(157, 194, 68, 29);
+		btnTb3.setBounds(157, 207, 68, 29);
 		contentPane.add(btnTb3);
 		
 		btnTb4 = new JButton("Tb4");
@@ -124,23 +108,16 @@ public class VistaTablaDB extends JFrame implements Vistas{
 				controlador.consultarQuery("SELECT * FROM world.users");
 			}
 		});
-		btnTb4.setBounds(224, 194, 68, 29);
+		btnTb4.setBounds(224, 207, 68, 29);
 		contentPane.add(btnTb4);
 		
 		btnAad = new JButton("Añadir");
-		btnAad.setBounds(292, 194, 136, 29);
+		btnAad.setBounds(292, 207, 136, 29);
 		contentPane.add(btnAad);
 		
-		btnVerSel = new JButton("Ver Selección");
-		btnVerSel.setBounds(292, 226, 136, 29);
-		contentPane.add(btnVerSel);
-		
-		scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(22, 226, 270, 40);
-		contentPane.add(scrollPane_1);
-		
-		txtSelec = new JTextPane();
-		scrollPane_1.setViewportView(txtSelec);
+		btnVerTablas = new JButton("Ver Tablas");
+		btnVerTablas.setBounds(292, 237, 136, 29);
+		contentPane.add(btnVerTablas);
 	}
 
 	public void setControlador(Object controlador) {
@@ -156,7 +133,5 @@ public class VistaTablaDB extends JFrame implements Vistas{
 		this.table.setModel(this.modeloTabla);
 	}
 
-	public void setTxtSelec(String seleccion) {
-		this.txtSelec.setText(seleccion);
-	}
 }
+
