c8b3867992d2a2025921678356cedd02a5a670a2
Ruben Ayllon Martinez <ruben.ayllon@live.u-tad.com>
2023-06-16 11:14:09 +0200

    commit correcion

--- ordinariaENDE_RAM/src/ejercicio2/OrdinariaENDE.java
+++ ordinariaENDE_RAM/src/ejercicio2/OrdinariaENDE.java
@@ -21,7 +21,7 @@ public class OrdinariaENDE {
     public static boolean esPalindromo(String palabra) {
         int longitud = palabra.length();
         for (int i = 0; i < longitud / 2; i++) {
-            if (palabra.charAt(i) == palabra.charAt(longitud - 1 - i)) {
+            if (palabra.charAt(i) != palabra.charAt(longitud - 1 - i)) {
                 return false;
             }
         }
